96a25715761a03da68f2b03ce2f9805eb4e44de1
lncd <willforan@gmail.com>
2015-04-24 18:49:37 -0400

    parallel and sort for xargs roistat

--- 03_roiMean.bash
+++ 03_roiMean.bash
@@ -3,13 +3,16 @@
 #
 # find each group in folder_mask_age.txt  and run roi stats with approprate mask
 #
-# output files to res/
+# output files to res/$grp.txt
 
+# what do the final processed runs look like
 subjdir="../ANTISTATELONG/"
+filepat="/run*/nfswkmt_functional_5.nii.gz"
+
 [ ! -d res ] && mkdir res
 for mask in masks/*+tlrc.HEAD; do
  grp=$(basename $mask +tlrc.HEAD)
- awk "(/$grp/){print \"$subjdir\" \$1 \"/run*/nfswkmt_functional_5.nii.gz\"}" txt/folder_mask_age.txt  | 
-  xargs 3dROIstats -prefix res/$grp.txt -mask $mask
+ awk "(/$grp/){print \"$subjdir\" \$1 \"$filepat\"}" txt/folder_mask_age.txt  | 
+   xargs -P5 -I{} bash -c "3dROIstats  -mask $mask {}"  | sort | uniq > res/$grp.txt
 done
 
--- ROIs.R
+++ ROIs.R
@@ -1,8 +1,11 @@
 # from table:
 # http://www.jneurosci.org/content/30/46/15535/T1.expansion.html
+# saved as txt/Kais26ROI.txt
 library(plyr)
 df <- read.table(sep="\t",'txt/Kais26ROI.txt')
 names(df)<-c('ROI','Grp','x','y','z')
 df$ROI <- df$ROI[(ceiling(1:nrow(df)/3)-1)*3+1 ]
 ddply(df,.(Grp),function(g) write.table(g[,c('x','y','z')],file=paste0('txt/',g$Grp[1],'_tal_xyz.1D'),row.names=F, quote=F, col.names=F) )
+
+write.table(df$ROI,row.names=T,quote=F,col.names=F,file='txt/roikey.txt')
 
